7ccf9c6dbc6ec6e9d9abba2f7cd4b9f0f9c52f9f
Aravind.Subbarao <aravind.subbarao@gds.ey.com>
2020-09-03 17:00:01 +0530

    Added npm scripts

--- cypress/integration/examples/GreenKart/Test2.js
+++ cypress/integration/examples/GreenKart/Test2.js
@@ -4,7 +4,7 @@ describe('My second Cypress test suite', function()
     it('second Cypress test case', function()
     {
         // test steps - handling checkboxes
-        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.visit("https://rahulshettyacademy.com/") //AutomationPractice
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value','option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check(['option2','option3'])
